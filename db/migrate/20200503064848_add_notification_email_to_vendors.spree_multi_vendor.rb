ff9cfb3e0b37f119f7f301500836ed633672416b
Dinesh Chohda <dinesh.skyach@gmail.com>
2020-05-03 16:59:46 +0530

    remove old code

--- db/migrate/20200503064848_add_notification_email_to_vendors.spree_multi_vendor.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from spree_multi_vendor (originally 20190322133128)
-class AddNotificationEmailToVendors < SpreeExtension::Migration[4.2]
-  def change
-    add_column :spree_vendors, :notification_email, :string
-  end
-end
