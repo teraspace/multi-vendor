a8cb1fa96975a6596c3769fbf7a5649ae7fcd9cf
javierburo18 <head-ruby@xelex.co>
2020-05-06 17:28:38 -0500

    spree_analytics_trackers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_02_102641) do
+ActiveRecord::Schema.define(version: 2020_05_06_222631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
