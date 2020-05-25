67ca012b0c133353a5077a05a8069b21d897c694
Pablo Murillo <pablo_murillo@sshonduras.com>
2020-05-25 09:47:01 -0600

    added production flag in dockerbuild

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,6 +53,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
   config.webpacker.check_yarn_integrity  = false
+
+  config.log_level = :info
+
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
   #   config.hosts << "multi-vendor.us-east-2.elasticbeanstalk.com"
