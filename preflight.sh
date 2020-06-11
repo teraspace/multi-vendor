985897f769a996a8c36f0585b4c2650f12eda302
Carlos Patiño <cpatino@fingeosel.com>
2020-06-11 13:32:31 -0500

    preflight

--- preflight.sh
+++ preflight.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 set -e
-bundle exec rails g duna_spree_sample:install
-bundle exec rake duna_spree_sample:clear_tables #pide input y/n
-bundle exec rails db:migrate
-bundle exec rake duna_spree_sample:load
-bundle exec rake spree_auth:admin:create      #pide ememail y password
-bundle exec rake spree_multi_vendor:sample:create
+echo 'preflighting start'
+rm -Rf tmp
+ln -s /tmp tmp
+#bundle exec rails g duna_spree_sample:install
+#bundle exec rake duna_spree_sample:clear_tables #pide input y/n
+#bundle exec rails db:migrate
+#bundle exec rake duna_spree_sample:load
+#bundle exec rake spree_auth:admin:create      #pide ememail y password
+#bundle exec rake spree_multi_vendor:sample:create
+echo 'preflighting end'
 exec "$@"
