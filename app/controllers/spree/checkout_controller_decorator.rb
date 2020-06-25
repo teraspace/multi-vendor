70910aad007daa42613f54b54db414823504d4a5
Carlos Patiño <cpatino@fingeosel.com>
2020-06-25 00:23:56 -0500

    address checkout

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -10,6 +10,16 @@ module Spree::CheckoutControllerDecorator
       redirect_to checkout_state_path(@order.state) and return
     end
   end
-end
 
+
+  def address_params
+
+    p params.require(:address).permit(permitted_address_attributes)
+    params.require(:address).permit(permitted_address_attributes)
+  end
+
+end
+Spree::PermittedAttributes.address_attributes << :lat
+Spree::PermittedAttributes.address_attributes << :lon
 Spree::CheckoutController.prepend(Spree::CheckoutControllerDecorator)
+
